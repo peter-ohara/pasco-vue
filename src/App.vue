1c5958342299448a5ae832c241e0700db510372d
amadu abdul <amadu abdul>
2017-09-14 19:45:44 +0000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- Don't drop "q-app" class -->
+  <div id="q-app">
+    <header>
+      <img src="~assets/logo3.png" alt="">
+    </header>
+    <main>
+<!--      <img src="~assets/quasar-logo-full.svg" alt="Quasar PWA">-->
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+/*
+ * Root component
+ */
+export default {}
+</script>
+
+<style lang="stylus">
+  @import '~variables'
+
+  main
+    text-align center
+    margin-top 0px
+
+  header
+    margin 0
+    height 60px
+    background-color $primary
+    box-shadow 0px 2px 20px 1px $neutral
+    img
+      padding 5px 10px
+      height 50px
+      margin 5px
+</style>
---
+++ src/components/main.vue
@@ -0,0 +1,86 @@
+<template>
+ <div class="main">
+    <q-card class="search-area">
+      <q-input
+        v-model="text"
+        placeholder="Find a test"
+        :loading="loading"/>
+    </q-card>
+    <div class="content">
+      <q-card class="card">
+        <div class="card-side">
+          <div class="card-icon">
+            <p>ES</p>
+          </div>
+        </div>
+        <div class="card-main">
+          <p class="card-title">
+            CSM 2016 Mid Semester Exam
+          </p>
+          <p class="duration">1hr</p>
+        </div>
+      </q-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  QInput,
+  QCard} from 'quasar'
+
+
+export default {
+  name: 'hello',
+  loading: true,
+  components: {
+    QInput,
+    QCard
+  }
+
+
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus">
+@import '~variables'
+
+.search-area
+  height 45px
+  background white
+  input
+    height 40px
+    padding 5px 10px 0px
+
+
+.content
+  margin-top 20px
+
+.card
+  background white
+  display flex
+  padding 10px
+
+
+.card-side
+  width 20%
+  max-width 200px
+  min-width 80px
+
+.card-icon
+  background-color $info
+  border-radius 50px
+  height 60px
+  width 60px
+  p
+    line-height 60px
+    color white
+    font-size 30px
+
+
+.card-title
+  text-align left
+</style>
