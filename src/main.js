b2433cb011679503fc9bd9538645eafd23af2f09
Peter O'Hara Adu <ohara.invent@gmail.com>
2017-09-25 13:10:08 +0000

    Include material icons from quasar-extras package

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import Vue from 'vue'
 import Quasar from 'quasar'
 import router from './router'
 import VueResource from 'vue-resource'
+import 'quasar-extras/material-icons'
 
 Vue.config.productionTip = false
 Vue.use(Quasar) // Install Quasar Framework
