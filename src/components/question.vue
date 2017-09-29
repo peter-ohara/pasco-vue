fc322b7d59c9f25f48af81f92b4527475acd9e2b
Peter O'Hara Adu <ohara.invent@gmail.com>
2017-09-29 12:38:43 +0000

    Remove clickable answers in question view

--- src/components/question.vue
+++ src/components/question.vue
@@ -7,14 +7,12 @@
 
     <div v-show="question.question_type !== 'header'" class="question-container">
       <p v-html="replaceInputs(question.question)" class="question"></p>
-      <q-option-group
-        v-show="question.choices && question.choices.length > 0"
-        class="choices-container"
-        color="blue"
-        type="radio"
-        v-model="answer"
-        :options="choices"
-      />
+      <ul class="choices-container" v-show="question.choices && question.choices.length > 0">
+        <li v-for="(choice, index) in question.choices" class="choices">
+          <p class="letter">{{ getChoiceLetter(index) }}</p>
+          <p class="choice" v-html="choice"></p>
+        </li>
+      </ul>
     </div>
 
     <q-inner-loading :visible="isPageLoading"></q-inner-loading>
@@ -44,22 +42,6 @@
     computed: {
       isPageLoading () {
         return this.$store.state.loadingUsersTests
-      },
-      choices () {
-        if (this.question.question_type === 'mcq') {
-          if (this.question.choices && this.question.choices.length > 0) {
-            let self = this
-            return this.question.choices.map(function (choice, index) {
-              return {
-                label: '<span class="letter">' + self.getChoiceLetter(index) + '</span><span class="choice">' + choice + '</span>',
-                value: index
-              }
-            })
-          }
-          return []
-        } else {
-          return []
-        }
       }
     },
     mounted () {
@@ -116,25 +98,23 @@
       .choices-container
         background-color white
         padding 5px 12px
-        margin 0 16px
-        div:nth-last-child(1) {
+        .choices:nth-last-child(1) {
           border-bottom: none;
         }
 
-        > div
+        .choices
           display flex
           margin-top 8px
           padding-left 8px
           padding-right 8px
-          padding-bottom 8px
           border-bottom 1px solid $light-gray
-          .q-option-label
+          p
             font-size 20px
-            .choice
-              color $mid-gray
-              text-align left
-            .letter
-              padding 0 25px 0 8px
-              color $blue
+          p.choice
+            color $mid-gray
+            text-align left
+          p.letter
+            padding 0 25px 0 8px
+            color $blue
 
 </style>
