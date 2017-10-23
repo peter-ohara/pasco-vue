517e0a662bed7ce352273cff9ef2102aadfbb2e3
Peter O'Hara Adu <ohara.invent@gmail.com>
2017-10-23 21:38:19 +0000

    Refactor authentication code

--- src/api/index.js
+++ src/api/index.js
@@ -1,47 +1,27 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import router from '../router'
-Vue.router = router
-
 Vue.use(VueResource)
 
-Vue.http.options.root = 'http://localhost:3000'
+Vue.router = router
 Vue.http.interceptors.push(function (request, next) {
   next(function (res) {
-
-    // Unauthorized Access
-
     if (res.status === 401 && ['Unauthorized'].indexOf(res.statusText) > -1) {
+      // Unauthorized Access
+
       Vue.auth.logout({
         redirect: {name: 'signIn'}
       })
-    }
+    } else if (res.status === 500) {
+      // System Error
 
-    // System Error
-
-    else if (res.status === 500) {
       Vue.router.push({name: 'error-500'})
     }
   })
 })
 
-Vue.use(require('@websanova/vue-auth'), {
-  auth: require('./bearer.js'),
-  http: require('@websanova/vue-auth/drivers/http/vue-resource.1.x.js'),
-  router: require('@websanova/vue-auth/drivers/router/vue-router.2.x.js'),
-  rolesVar: 'type',
-  authRedirect: {path: '/signin'},
-  forbiddenRedirect: {path: '/403'},
-  notFoundRedirect: {path: '/404'},
-  registerData: {url: 'auth/register', method: 'POST', redirect: '/signin'},
-  loginData: {url: 'auth/login', method: 'POST', redirect: '/', fetchUser: false},
-  logoutData: {url: 'auth/logout', method: 'POST', redirect: '/', makeRequest: false},
-  oauth1Data: {url: 'auth/login', method: 'POST'},
-  fetchData: {url: 'auth/user', method: 'GET', enabled: true},
-  refreshData: {url: 'auth/refresh', method: 'GET', enabled: true, interval: 30}
-})
-
-const API_ROOT = 'https://pasco-api-staging.herokuapp.com/'
+// Vue.http.options.root = 'https://pasco-api-staging.herokuapp.com/'
+Vue.http.options.root = 'http://localhost:3000'
 const QuizResource = Vue.resource('quizzes{/id}?include=questions')
 const UserResource = Vue.resource('auth/user')
 
--- src/main.js
+++ src/main.js
@@ -21,8 +21,24 @@ import Vuelidate from 'vuelidate'
 Vue.config.productionTip = false
 Vue.use(Quasar) // Install Quasar Framework
 Vue.use(VueResource) // Install Vue Resource Framework
-Vue.use(VueClipboard)
-Vue.use(Vuelidate) //Form validation
+Vue.use(VueClipboard) // Copying links onclick
+Vue.use(Vuelidate) // Form validation
+
+Vue.use(require('@websanova/vue-auth'), {
+  auth: require('./auth/bearer.js'),
+  http: require('@websanova/vue-auth/drivers/http/vue-resource.1.x.js'),
+  router: require('@websanova/vue-auth/drivers/router/vue-router.2.x.js'),
+  rolesVar: 'type',
+  authRedirect: {path: '/signin'},
+  forbiddenRedirect: {path: '/403'},
+  notFoundRedirect: {path: '/404'},
+  registerData: {url: 'auth/register', method: 'POST', redirect: '/signin'},
+  loginData: {url: 'auth/login', method: 'POST', redirect: '/', fetchUser: false},
+  logoutData: {url: 'auth/logout', method: 'POST', redirect: '/', makeRequest: false},
+  oauth1Data: {url: 'auth/login', method: 'POST'},
+  fetchData: {url: 'auth/user', method: 'GET', enabled: true},
+  refreshData: {url: 'auth/refresh', method: 'GET', enabled: true, interval: 30}
+})
 
 if (__THEME === 'mat') {
   // require('quasar-extras/roboto-font')
