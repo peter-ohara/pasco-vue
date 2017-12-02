b198f2a772dd6ebef7dd23aa360a08f8780e6438
Peter O'Hara Adu <ohara.invent@gmail.com>
2017-12-02 09:36:27 +0000

    Render html in explanations

--- src/components/explanation.vue
+++ src/components/explanation.vue
@@ -5,7 +5,7 @@
         We don't have an explanation to for this question at the moment.
         However, you can see what other users are saying about this question in the comments section below
       </p>
-      <p>{{question.explanation}}</p>
+      <p v-html="question.explanation"></p>
     </div>
   </div>
 </template>
