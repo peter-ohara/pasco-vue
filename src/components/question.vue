dc427d634c9dbc894d23ee6ac4b0ab6d08e247e1
crazyabdul <frost3003@gmail.com>
2017-12-10 21:53:23 +0000

    Loading icon now shows as image placeholders in questions

--- src/components/question.vue
+++ src/components/question.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="single-question">
 
-    <div v-show="question.question_type === 'header'" class="header-container">
-      <p v-html="question.content"></p>
+    <div v-if="question.question_type === 'header'" class="header-container">
+      <p class="question-content" v-html="question.content"></p>
     </div>
 
-    <div v-show="question.question_type !== 'header'" class="question-container">
-      <p v-html="replaceInputs(question.question)" id="questions" class="question"></p>
+    <div v-if="question.question_type !== 'header'" class="question-container">
+      <p v-html="replaceInputs(question.question)" id="questions" class="question question-content"></p>
       <q-option-group
         v-show="question.choices && question.choices.length > 0"
         class="choices-container"
@@ -17,7 +17,6 @@
         @change="checkAnswer(answer)"
       />
     </div>
-
   </div>
 </template>
 
@@ -48,10 +47,16 @@
         correctAnswerAlert: false,
         wrongAnswerAlert: false,
         noAnswerAlert: false,
+        imageSize: 0,
+        interval: null,
+        imageElem: null
         // correctAnswerSound: '../../../assets/ding.wav'
       }
     },
     computed: {
+      contentElem () {
+        return document.querySelector('.question-content')
+      },
       choices () {
         if (this.question.question_type === 'mcq') {
           if (this.question.choices && this.question.choices.length > 0) {
@@ -71,9 +76,16 @@
     },
     mounted () {
       this.renderMath()
+      this.isThereImage()
     },
     beforeDestroy () {
       this.dismissAlerts()
+      console.log("before Destroy")
+    },
+    updated() {
+      //clear any rogue interval
+      clearInterval(this.interval)
+      this.isThereImage()
     },
     watch: {
       question: function () {
@@ -82,6 +94,58 @@
       }
     },
     methods: {
+      isThereImage() {
+        this.imageElem = document.querySelector('.question-content img')
+        //If Image exists, append loading icon
+        if(this.imageElem) {
+          this.appendLoadingIcon();
+          this.runInterval();
+        }
+      },
+      hasImageLoaded(img) {
+        //console.log(img.naturalWidth)
+        if (!img.complete) {
+          return false;
+        }
+
+        if (img.naturalWidth === 0) {
+          return false;
+        }
+
+        // No other way of checking: assume it’s ok.
+        return true;
+      },
+      appendLoadingIcon () {
+        //Create Element
+        let span = document.createElement('span');
+
+        //Set class
+        span.setAttribute('class', 'icon-wrap')
+
+        //Insert Icon
+        span.innerHTML = `<svg v-if="hasImageLoaded(imageElem)" class="icon" style="width:24px;height:24px" viewBox="0 0 24 24">
+          <path fill="#000000" d="M12,4V2A10,10 0 0,0 2,12H4A8,8 0 0,1 12,4Z" />
+        </svg>`
+
+        //console.log(this.contentElem)
+        //Append element
+        this.contentElem.appendChild(span)
+        //console.log(this.contentElem)
+      },
+      removeLoadingIcon () {
+        const iconWrap = document.querySelector('.question-content .icon-wrap');
+        //console.log(iconWrap, "REMOVE")
+        this.contentElem.removeChild(iconWrap);
+      },
+      runInterval () {
+        const self = this
+        this.interval = setInterval(function(){
+          if(self.hasImageLoaded(self.imageElem) ){
+            self.removeLoadingIcon();
+            clearInterval(self.interval)
+          }
+        }, 10)
+      },
       renderMath: function () {
         this.$nextTick(function () {
           // eslint-disable-next-line no-undef,eqeqeq
@@ -152,6 +216,7 @@
   export default pageData
 </script>
 
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <!-- Intentionally unscoped due to selecting .q-* classes that are only available after compilation -->
 <style lang="stylus">
@@ -205,5 +270,18 @@
             .letter
               padding 0 25px 0 8px
               color $blue
-
+    .question-content
+      .icon-wrap
+        width 100px
+        height 100px
+        background #d0d0d0
+        display block
+        border-radius 5px
+      .icon
+        margin 35px 35px
+        -webkit-animation:spin 1s linear infinite;
+        -moz-animation:spin 1s linear infinite;
+        animation:spin 1s linear infinite;
+      img
+        box-shadow 0px 0px 5px 0px #7b7b7b
 </style>
